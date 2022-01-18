66460eef0373876adb2fc8758a193ea73ddb31fb
NoeMbe <noemie.mbemba@hotmail.fr>
2022-01-18 16:12:07 +0100

    noemie2

--- assets/espace.js
+++ assets/espace.js
@@ -1,10 +1,25 @@
 import './styles/espace.scss';
 import './bootstrap';
 
-$(".profile").addClass("pre-enter");
-setTimeout(function(){
-	$(".profile").addClass("on-enter");
-}, 500);
-setTimeout(function(){
-	$(".profile").removeClass("pre-enter on-enter");
-}, 3000);
+const indicator = document.querySelector('.nav-indicator');
+const items = document.querySelectorAll('.nav-item');
+
+function handleIndicator(el) {
+  items.forEach(item => {
+    item.classList.remove('is-active');
+    item.removeAttribute('style');
+  });
+  
+  indicator.style.width = `${el.offsetWidth}px`;
+  indicator.style.left = `${el.offsetLeft}px`;
+  indicator.style.backgroundColor = el.getAttribute('active-color');
+
+  el.classList.add('is-active');
+  el.style.color = el.getAttribute('active-color');
+}
+
+
+items.forEach((item, index) => {
+  item.addEventListener('click', (e) => { handleIndicator(e.target)});
+  item.classList.contains('is-active') && handleIndicator(item);
+});

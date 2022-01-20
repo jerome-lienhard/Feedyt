f25ca0c09cbb25fd1ec21113e36aac6a3c445378
NoeMbe <noemie.mbemba@hotmail.fr>
2022-01-20 16:05:19 +0100

    noemie

--- webpack.config.js
+++ webpack.config.js
@@ -75,10 +75,10 @@ Encore
 //.enableIntegrityHashes(Encore.isProduction())
 
 // uncomment if you're having problems with a jQuery plugin
-.autoProvidejQuery().autoProvideVariables({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery'
-}) 
+.autoProvidejQuery()// require the JavaScript
+require('bootstrap-star-rating');
+// require 2 CSS files needed
+require('bootstrap-star-rating/css/star-rating.css');
+require('bootstrap-star-rating/themes/krajee-svg/theme.css');
 
 module.exports = Encore.getWebpackConfig();

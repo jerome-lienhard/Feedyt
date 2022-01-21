025e17924ae826187880f94c5e5a46bef1e9ea00
shamtreck <email.x.borel@laposte.net>
2022-01-21 10:04:17 +0100

    xavier

--- src/Controller/EspaceController.php
+++ src/Controller/EspaceController.php
@@ -29,11 +29,7 @@ class EspaceController extends AbstractController
         $actu = $user->getFollows();
         $news = new Flux;
         $Tabnews = Null;
-<<<<<<< HEAD
         $menu= Null;
-=======
-        $menu = null;
->>>>>>> 7e76c24930fd514e3f38f7b46d6a4af81e9e9dec
 
        /* foreach ($actu as $flu) {
             $affiche = $flu->getSite()->getSourceUrl();
@@ -50,16 +46,8 @@ class EspaceController extends AbstractController
             }
 
             if ($flux != false) {
-<<<<<<< HEAD
                 if($menu ==null);
                 return $this->render('espace/index.html.twig', ["flux" => $flux, "url_ajout" => $request, "news" => $Tabnews]);
-=======
-               if($menu==null){
-                return $this->render('espace/index.html.twig', ["flux" => $flux, "url_ajout" => $request, "news" => $Tabnews, "menu" => $menu]);
-               }
-                return $this->render('espace/index.html.twig', ["flux" => $flux, "url_ajout" => $request, "news" => $Tabnews]);
-
->>>>>>> 7e76c24930fd514e3f38f7b46d6a4af81e9e9dec
             }
             // Sinon s'il y a une demande d'ajout à la liste des suivi de flux
         } elseif ($rq->query->get("ajout_url")) {

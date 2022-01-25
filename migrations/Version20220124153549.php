b1b89c696aa93afc4b8e40abf19cee0d63adec63
jlienhard <jlienhard2@gmail.com>
2022-01-25 10:46:41 +0100

    jerome

--- migrations/Version20220124153549.php
+++ migrations/Version20220124153549.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220121125254 extends AbstractMigration
+final class Version20220124153549 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220121125254 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE follow ADD categorie VARCHAR(100) DEFAULT NULL');
+        $this->addSql('ALTER TABLE follow ADD name VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE follow DROP categorie');
+        $this->addSql('ALTER TABLE follow DROP name');
     }
 }
